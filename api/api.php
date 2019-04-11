b4816246a7387e7f9685d1a00ee688cbdc116fc5
vahejab <vahe.jabagchourian@gmail.com>
2019-04-10 22:36:12 -0700

    Fixed .htaccess and added inital api endpoints (api.php)

--- api/api.php
+++ api/api.php
@@ -1,2 +1,22 @@
 <?php
- echo $_SERVER['REQUEST_METHOD'];
+    $requestURI = $_GET;
+    
+    $method = strtolower($_SERVER['REQUEST_METHOD']);
+    switch ($method){
+        case 'get':
+             //var_dump($_GET);
+             echo json_encode($requestURI);
+            break;
+        case 'post':
+            echo json_encode($requestURI);
+            break;
+        case 'put':
+            echo json_encode($requestURI);
+            break;
+        case 'delete':
+            echo json_encode($requestURI);
+            break;
+        default:
+            // unimplemented method
+            http_response_code(405);
+    }
